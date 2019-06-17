25136a8bb000575afafdc138314453e33affcc37
Doberjohn <johnfanidis@gmail.com>
2019-06-17 23:39:20 +0300

    initial commit

---
+++ client/src/components/App/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from "../Header/Header";
+import StreamCreate from "../streams/StreamCreate/StreamCreate";
+import StreamEdit from "../streams/StreamEdit/StreamEdit";
+import StreamDelete from "../streams/StreamDelete/StreamDelete";
+import StreamList from "../streams/StreamList/StreamList";
+import StreamShow from "../streams/StreamShow/StreamShow";
+
+const App = () => {
+    return (
+        <div className="ui container">
+            <BrowserRouter>
+                <div>
+                    <Header/>
+                    <Route path="/" exact component={StreamList}></Route>
+                    <Route path="/streams/new" component={StreamCreate}></Route>
+                    <Route path="/streams/edit" component={StreamEdit}></Route>
+                    <Route path="/streams/delete" component={StreamDelete}></Route>
+                    <Route path="/streams/show" component={StreamShow}></Route>
+                </div>
+            </BrowserRouter>
+        </div>
+    )
+}
+
+export default App;
---
+++ client/src/components/streams/StreamCreate/StreamCreate.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class StreamCreate extends Component {
+    renderError({ error, touched}) {
+        if (touched && error) {
+            return (
+                <div className="ui error message">
+                    <div className="header">{error}</div>
+                </div>
+            )
+        }
+    }
+
+    // renderInput normally takes a formProps object as a parameter.
+    // This should normally be renderInput(formProps) {
+    // Here we are destructuring the input object out of the formProps object.
+    // In essence we are doing something like var { input } = formProps.
+    renderInput = ({input, label, meta}) => {
+        const className = `field ${meta.error && meta.touched ? 'error' : ''}`
+        return (
+            // Here we are assigning all properties of formProps.input (destructured input)
+            // to the input that is being returned. This properties contain value, onChange, onBlur handlers
+            // and various other useful stuff for the input.
+            <div className={className}>
+                <label>{label}</label>
+                <input {...input} autoComplete="off"/>
+                {this.renderError(meta)}
+            </div>
+        );
+    }
+
+    onSubmit(formValues) {
+        console.log(formValues);
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
+                <Field name="title" label="Enter title" component={this.renderInput} />
+                <Field name="description" label="Enter description" component={this.renderInput} />
+
+                <button className="ui button primary">Submit</button>
+            </form>
+        )
+    }
+}
+
+const validate = formValues => {
+    const errors = {};
+
+    if (!formValues.title) {
+        errors.title = 'You must enter a title';
+    }
+
+    if (!formValues.description) {
+        errors.description = 'You must enter a description';
+    }
+
+    return errors;
+}
+
+export default reduxForm({
+    form: 'streamCreate',
+    validate
+})(StreamCreate);
